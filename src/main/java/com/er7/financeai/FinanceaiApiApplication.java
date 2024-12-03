2753470ba55fcbce1cb6e4ecde5443fe088a4016
CarlosEReis <carlos.er7@gmail.com>
2024-12-03 09:20:00 -0300

    build: project base

---
+++ src/main/java/com/er7/financeai/FinanceaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.er7.financeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceaiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/er7/financeai/FinanceaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.er7.financeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
