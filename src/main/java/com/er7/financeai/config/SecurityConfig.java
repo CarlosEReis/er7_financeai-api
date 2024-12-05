ab2e7598e09daf91a5d1e085cd2efaa41b20c931
CarlosEReis <carlos.er7@gmail.com>
2024-12-05 13:10:56 -0300

    feat(config): config Security

--- src/main/java/com/er7/financeai/config/SecurityConfig.java
+++ src/main/java/com/er7/financeai/config/SecurityConfig.java
@@ -14,14 +14,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/v1/transactions").authenticated()
-                    .anyRequest().authenticated()
-            )
-            .cors(t -> t.configure(http))
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults()))
-            .build();
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/v1/transactions", "/create-checkout-session").authenticated()
+                        .requestMatchers("/webhook").permitAll()
+                )
+                .cors(t -> t.configure(http))
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/webhook")  // Desabilita CSRF para o webhook
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
+                .build();
     }
 
 }
