3c484018b76a6f13d3986d6a1da9c12160333b66
CarlosEReis <carlos.er7@gmail.com>
2025-07-15 19:43:20 -0300

    feat(config): add log cors console

--- src/main/java/com/er7/financeai/config/CorsConfig.java
+++ src/main/java/com/er7/financeai/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.er7.financeai.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfig.class);
+
     @Value("${com.er7.financeai.api.cors.allowedOrigins}")
     private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        LOGGER.info("CORS allowed origins configuration: {}", allowedOrigins);
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
