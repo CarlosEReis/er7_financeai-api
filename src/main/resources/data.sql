4520550ad6832eba12fbad651115938e81aa333c
CarlosEReis <carlos.er7@gmail.com>
2025-07-16 17:20:28 -0300

    build(data): create file schema and base data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.payment_method (id, name) VALUES
+(1, 'CREDIT_CARD'),
+(2, 'DEBIT_CARD'),
+(3, 'BANK_TRANSFER'),
+(4, 'BANK_SLIP'),
+(5, 'CASH'),
+(6, 'PIX'),
+(7, 'OTHER'),
+(8, 'CRIPTO');
+
+INSERT INTO public.transaction_category (id, name) VALUES
+(1, 'HOUSING'),
+(2, 'TRANSPORTATION'),
+(3, 'FOOD'),
+(4, 'ENTERTAINMENT'),
+(5, 'HEALTH'),
+(6, 'UTILITY'),
+(7, 'SALARY'),
+(8, 'EDUCATION'),
+(9, 'OTHER');
+
+
+
+
