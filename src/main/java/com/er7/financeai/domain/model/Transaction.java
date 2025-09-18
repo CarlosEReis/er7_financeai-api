a8902750388d87bd9612010ace1bd12d547da682
CarlosEReis <carlos.er7@gmail.com>
2025-09-18 18:58:54 -0300

    refactor(domain): Transaction

--- src/main/java/com/er7/financeai/domain/model/Transaction.java
+++ src/main/java/com/er7/financeai/domain/model/Transaction.java
@@ -1,19 +1,25 @@
 package com.er7.financeai.domain.model;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
 @Entity
+@Table(name = "transactions")
+@SQLDelete(sql = "UPDATE transactions SET deleted = true WHERE id = ?")
+@SQLRestriction(value = "deleted = false")
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Boolean deleted = false;
 
     @Enumerated(EnumType.STRING)
     private TransactionType type;
@@ -27,16 +33,24 @@ public class Transaction {
     @Column(name = "date_transaction")
     private OffsetDateTime date;
 
+    @NotNull
     @CreationTimestamp
     private OffsetDateTime createdAt;
 
+    @NotNull
     @UpdateTimestamp
     private OffsetDateTime updatedAt;
 
     @ManyToOne
     private PaymentMethod paymentMethod;
 
-    private String userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
 
     public Long getId() {
         return id;
@@ -44,6 +58,14 @@ public class Transaction {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
     }
 
     public TransactionType getType() {
@@ -110,12 +132,20 @@ public class Transaction {
         this.paymentMethod = paymentMethod;
     }
 
-    public String getUserId() {
-        return userId;
+    public User getUser() {
+        return user;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
     }
 
     @Override
